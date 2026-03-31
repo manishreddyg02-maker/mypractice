65539ffa911d46f7e8875f4afd0498469542e802
Manish <manishreddyg02@gmail.com>
2026-04-01 01:09:38 +0530

    Printing without using printf

--- program1.c
+++ program1.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+#include<unistd.h>
+#include<string.h>
 int main()
 {
-    printf("Hello Manish\n");
+    char buff[100];
+    write(1,"Enter the text to be printed\n",30);
+    fgets(buff, 100, stdin);
+    write(1, buff, strlen(buff));
     return 0;
 }
